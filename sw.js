4ee271f52893f63d5d18553e48567512ac7f7c6a
rekojel-hash <lamirel.charles@gmail.com>
2026-06-10 22:33:01 +0200

    Update service worker cache and assets
    Updated cache version and added more assets to cache.

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
-const CACHE = 'tirpro-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'tirpro-v1.1'; // Pense à changer la version ici à chaque grosse mise à jour
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .catch(err => console.error('Erreur de mise en cache:', err))
+  );
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
